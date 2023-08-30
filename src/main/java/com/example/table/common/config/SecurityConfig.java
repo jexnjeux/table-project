5b95bcc12b5f32df454e54a896cdaf5420ae018a
jexnjeux <zieun.na@gmail.com>
2023-08-30 21:22:08 +0900

    feat: SecurityConfig fiterChain 업데이트, bCryptPasswordEncoder 추가

--- src/main/java/com/example/table/common/config/SecurityConfig.java
+++ src/main/java/com/example/table/common/config/SecurityConfig.java
@@ -6,17 +6,48 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer.FrameOptionsConfig;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
   @Bean
+  public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
+    UserDetails user = User.withUsername("user")
+        .password("{noop}password")
+        .roles("USER")
+        .build();
+
+    UserDetails partner = User.withUsername("partner")
+        .password("{noop}password")
+        .roles("PARTNER")
+        .build();
+
+    return new InMemoryUserDetailsManager(user, partner);
+  }
+
+  @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
     http.csrf(AbstractHttpConfigurer::disable)
-        .authorizeRequests()
-        .anyRequest().permitAll();
+        .authorizeRequests(authorizeRequests ->
+            authorizeRequests
+                .requestMatchers(new AntPathRequestMatcher("/api/partner/**")).hasRole("PARTNER")
+                .requestMatchers(new AntPathRequestMatcher("/api/user/**")).authenticated()
+                .anyRequest().permitAll()
+        );
+    http.formLogin(httpSecurityFormLoginConfigurer -> httpSecurityFormLoginConfigurer
+        .defaultSuccessUrl("/")
+        .failureUrl("/login?error")
+        .usernameParameter("username")
+        .passwordParameter("password")
+        .loginProcessingUrl("/api/login")
+        .permitAll());
     http.headers(headers -> headers
         .frameOptions(FrameOptionsConfig::sameOrigin
         )
@@ -24,5 +55,9 @@ public class SecurityConfig {
     return http.build();
   }
 
+  @Bean
+  public BCryptPasswordEncoder bCryptPasswordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
 
 }
